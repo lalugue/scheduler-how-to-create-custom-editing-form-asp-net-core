764fdeecbf769560f07a46328953d3c20ace493c
lalugue <51893731+lalugue@users.noreply.github.com>
2021-06-11 19:19:38 +0800

    update scheduler datasource

--- Models/SampleData.cs
+++ Models/SampleData.cs
@@ -28,21 +28,21 @@ namespace ASP_NET_Core.Models {
                 EndDate =  new DateTime(2015, 4, 25, 13, 2, 0),
                 Text = "Royal Wedding",
                 Director = "Stanley Donen",
-                Year = 1940,
-                Image = "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/movies/HisGirlFriday.jpg",
-                Duration = 92
+                Year = 1951,
+                Image = "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/movies/RoyalWedding.jpg",
+                Duration = 93
             },
             new SampleAppointment
             {
                 ID = 3,
-                Price = 10,
-                StartDate = new DateTime(2015, 4, 25, 9, 10, 0),
-                EndDate =  new DateTime(2015, 4, 25, 11, 1, 0),
-                Text = "His Girl Friday",
-                Director = "Howard Hawks",
-                Year = 1940,
-                Image = "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/movies/HisGirlFriday.jpg",
-                Duration = 92
+                Price = 15,
+                StartDate = new DateTime(2015, 4, 25, 13, 20, 0),
+                EndDate =  new DateTime(2015, 4, 25, 15, 21, 0),
+                Text = "A Star Is Born",
+                Director = "William A. Wellman",
+                Year = 1937,
+                Image = "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/movies/AStartIsBorn.jpg",
+                Duration = 111
             }
         };
     }
