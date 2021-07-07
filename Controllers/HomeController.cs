8b519df50ecb18c7001a311765b2a9b46e49903e
lalugue <51893731+lalugue@users.noreply.github.com>
2021-07-07 19:46:16 +0800

    return view with model data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP_NET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core.Controllers
@@ -10,11 +11,12 @@ namespace ASP_NET_Core.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(SampleData.Appointments);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View();
         }
     }
